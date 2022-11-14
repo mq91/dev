5852e566b164a70171bf6657941e4aab61504190
mohammed abdullah <maqarni@hotmail.com>
2022-11-14 07:51:43 +0000

    new file:   .vscode/settings.json 	new file:   webhook/__pycache__/main.cpython-310.pyc modified:   webhook/main.py

--- webhook/main.py
+++ webhook/main.py
@@ -1,3 +1,15 @@
-import os
+from typing import Union
 
-print(os.system('ipconfig'))
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
